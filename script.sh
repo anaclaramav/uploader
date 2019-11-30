f57d4be0284a400897c7b93ea9a2867b2f054f78
anaclaramav <anaclara.mav@hotmail.com>
2019-11-30 16:18:37 -0300

    adding script to install docker

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+apt-get update
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 apt-get install curl
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get install nodejs
